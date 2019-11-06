8bb26d0a7feeb9ad1c5b9cabb144efa801067b43
iDash3 <idash3.b@gmail.com>
2019-11-06 00:36:38 -0600

    Complete migration to pathlib

--- AutoDownloadsManager.py
+++ AutoDownloadsManager.py
@@ -1,6 +1,7 @@
 # Install from pip
-from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+from pathlib import Path, PureWindowsPath
 import time
 import sys
 import os
@@ -8,8 +9,8 @@ import os
 class MyHandler(FileSystemEventHandler):
   ### Private variables
   def __init__(self, folder_src, folder_destination):
-    self.folder_src = folder_src
-    self.folder_destination = folder_destination
+    self.folder_src = Path(folder_src)
+    self.folder_destination = Path(folder_destination)
     self.image_types = ('png', 'jpg', 'jpeg')
     self.doc_types = ('pdf', 'txt', 'docx', 'doc')
     self.exe_types = ('exe', 'bat', 'config')
@@ -18,16 +19,17 @@ class MyHandler(FileSystemEventHandler):
   ### Methods for handling file events
   def on_created(self, event):
     if not event.is_directory:
-      _, event_file = os.path.split(event.src_path)
-      file_type = event_file.split('.')[1]
-      file_name = event_file.split('.')[0]
-      if len(file_name.split('_')) > 1:
-        file_name_ = file_name.split('_')[1]
-        file_content = file_name.split('_')[0]
-        print(file_name_, file_content)
+      event_file = Path(event.src_path)
+      event_file_suffix = event_file.suffix
+      event_file_name = event_file.stem
+
+      if len(event_file_name.split('_')) > 1:
+        file_content = event_file_name.split('_')[0]
+        file_name = event_file_name.split('_')[1]
+        print(file_name, file_content)
+        self.__move_special(file_content, file_name)
       else:
-        pass
-      self.__move_file(file_name, file_type)
+        self.__move_file(event_file_name, event_file_suffix)
     else:
       # Its a directory
       pass
@@ -46,6 +48,8 @@ class MyHandler(FileSystemEventHandler):
   # Add Logging (?)
   # Dynamically change name (?)
   def __move_file(self, name, type):
+    complete_file_name =  name + type
+    type = type.split('.')[1]
     folder_name = 'Other shit'
     if(type in self.image_types):
       folder_name = 'Images'
@@ -54,14 +58,21 @@ class MyHandler(FileSystemEventHandler):
     elif(type in self.exe_types):
       folder_name = 'Executables'
 
-    file_path = self.folder_src + '/{}.{}'.format(name, type)
-    folder_path = self.folder_destination + '/' + folder_name
-    new_file_path = folder_path + '/{}.{}'.format(name, type)
+    file_path = self.folder_src / complete_file_name 
+    folder_path = self.folder_destination / folder_name 
+    new_file_path = folder_path / complete_file_name
     if not os.path.exists(folder_path): os.makedirs(folder_path)
     try:
       os.rename(file_path,new_file_path)
     except PermissionError:
-      pass
+      print("PERMISSION ERROR")
+    except FileNotFoundError:
+      print("FILENOTFOUND")
+    except FileExistsError:
+      print("FILEEXIST")
+  
+  def __move_special(self, content, name):
+    pass
 
   ### To add new especial cases, insert a dictonary of couples
   ## example = {id: (name, path)}
@@ -75,16 +86,18 @@ if __name__ == '__main__':
   # Get path from line arguments
   # path = sys.argv[1] if len(sys.argv) > 1 else "."
 
+  ## Check for compatibility on Linux and Mac
   folder_to_track = './SRCDownloads'
   folder_to_go = './Downloads'
   event_handler = MyHandler(folder_to_track, folder_to_go)
+
   especial_cases = {
-    'mn': ('MétodosNuméricos', r'D://'), 
-    'ed': ('EcuacionesDiferenciales', r'D://'),
-    'pye': ('ProbabilidadEstadística', r'D:\iDash\Documents\School\LiteralGrill\Probabilidad y Estadísitica'),
-    'ec': ('Economia', r''),
-    'al': ('AlgebraLineal', r''),
-    'qm': ('QuimicaMateriales', r''),
+    'mn': ('MétodosNuméricos', PureWindowsPath('')), 
+    'ed': ('EcuacionesDiferenciales', PureWindowsPath('')),
+    'pye': ('ProbabilidadEs', PureWindowsPath('')),
+    'ec': ('Economia', PureWindowsPath('')),
+    'al': ('AlgebraLineal', PureWindowsPath('')),
+    'qm': ('QuimicaMateriales', PureWindowsPath('')),
   }
 
   observer = Observer()
